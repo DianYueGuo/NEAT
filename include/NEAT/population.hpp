3fb284be5d6a0f513f66237445ab0e8ca8fcd7d1
郭典約 <57594637+DianYueGuo@users.noreply.github.com>
2025-12-06 19:38:31 +0800

    Update population.hpp

--- include/NEAT/population.hpp
+++ include/NEAT/population.hpp
@@ -50,7 +50,8 @@ class Population{
 		void speciate(int target = 5, int targetThresh = 0, float stepThresh = 0.5f, float a = 1.0f, float b = 1.0f, float c = 0.4f);
 		void crossover(bool elitism = false);	// git error#3
 		void mutate(float mutateWeightThresh = 0.8f, float mutateWeightFullChangeThresh = 0.1f, float mutateWeightFactor = 1.2f, float addConnectionThresh = 0.05f, int maxIterationsFindConnectionThresh = 20, float reactivateConnectionThresh = 0.25f, float addNodeThresh = 0.03f, int maxIterationsFindNodeThresh = 20);
-		void drawNetwork(int genomeId, sf::Vector2u windowSize = {1300, 800}, float dotsRadius = 6.5f);
+				// Disabled drawing for this integration.
+                void drawNetwork();
 		void printInfo(bool extendedGlobal = false, bool printSpecies = false, bool printGenomes = false, bool extendedGenomes = false);
 		void save(const std::string filepath = "./neat_backup.txt");
 		void load(const std::string filepath = "./neat_backup.txt");
